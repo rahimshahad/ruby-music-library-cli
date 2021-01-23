3f34e2e8474412788f9ed1f89dcbde518d1b7daa
rahimshahad <rahim.shahad2@gmail.com>
2021-01-23 01:55:08 -0500

    Done.

---
+++ lib/MusicImporter.rb
@@ -0,0 +1,14 @@
+class MusicImporter
+    def initialize(path)
+        @path = path
+    end
+    def path
+        @path
+    end
+    def files
+        Dir.entries(@path).select! {|entry| entry.end_with?(".mp3")}
+    end
+    def import
+        files.each {|file| Song.create_from_filename(file) }
+    end
+end
